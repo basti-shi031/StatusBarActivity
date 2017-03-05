19e9eb0581e3b467acac34334b60210a12be02c3
Boateng17 <876572078@qq.com>
2017-03-06 00:11:27 +0800

    init

--- app/src/main/java/com/basti/statusbaractivity/MainActivity.java
+++ app/src/main/java/com/basti/statusbaractivity/MainActivity.java
@@ -3,6 +3,7 @@ package com.basti.statusbaractivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 }
